f82684f051aa972227a8468c309088cfd85104c3
Alreyesh <alfreyh@gmail.com>
2022-01-23 15:16:21 -0500

    update: app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,31 @@ import { AppRoutingModule } from './app-routing-module/app-routing-module.module
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { SemanaTresComponent } from './semana-tres/semana-tres.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HistorialComprasComponent } from './historial-compras/historial-compras.component';
 import { RegisterComponent } from './register/register.component';
 import { UpdateComponent } from './update/update.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+<<<<<<< HEAD
 import { ComplaintsBookComponent } from './complaints-book/complaints-book.component';
+=======
+import { ProductListComponent } from './products/components/list/product-list.component';
+>>>>>>> dcb0e66857fd4ac32bab62f8dd9d6fd018217d4b
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    SemanaTresComponent,
     HistorialComprasComponent,
     RegisterComponent,
     UpdateComponent,
     AboutusComponent,
+<<<<<<< HEAD
     ComplaintsBookComponent,
+=======
+    ProductListComponent,
+>>>>>>> dcb0e66857fd4ac32bab62f8dd9d6fd018217d4b
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
