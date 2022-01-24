561865062e2797b96f29869255f1c394fbeb317f
yersonlinancervantes <64672117+yersonlinancervantes@users.noreply.github.com>
2022-01-23 20:05:51 -0500

    Productos
    Productos listado y registrar

--- src/app/admin/registrar-productos/registrar-productos.component.ts
+++ src/app/admin/registrar-productos/registrar-productos.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-productos',
   templateUrl: './registrar-productos.component.html',
   styleUrls: ['./registrar-productos.component.css']
 })
+  
 export class RegistrarProductosComponent implements OnInit {
+    productForm = this.fb.group({
+    categoria: ['', Validators.required],
+    nombre: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    precio: [0, [Validators.min(1), Validators.required]],
+    imagen: ['', Validators.required],
+    stock: [0, [Validators.min(1), Validators.required]]
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  __onSubmit() {
+    if(this.productForm.valid) {
+      console.log(this.productForm.value);
+      alert("Los datos fueron correctamente registrados");
+    } else {
+      alert("Complete todos los campos");
+    }
+  }
 
   ngOnInit(): void {
   }
