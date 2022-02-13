b8f910aebac3342b7f17dcb1d70e61a5b890abdc
Alreyesh <alfreyh@gmail.com>
2022-02-13 03:54:39 -0500

    update service user

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,35 @@ export class LoginComponent implements OnInit {
     usuario:['',[Validators.required,Validators.email]],
     password:['',Validators.required]
   });
-  constructor( private fb:FormBuilder) { }
+  constructor(
+    private fb:FormBuilder,
+    private readonly us: UserService,
+    private router: Router
+    ) { }
+  __login(data:any){
+    this.us.__login(data).subscribe((rest: any)=>{
+         alert(rest);
+
+        
+        if(rest.email == this.usuario && rest.password == this.password){
+          alert("Logeaste");
+          /*  sessionStorage.setItem('user',rest.name+' '+rest.lastName);
+            this.router.navigateByUrl('/home',{skipLocationChange:false}).then(
+              ()=>{
+                this.router.navigate(['home']);
+                window.location.reload();
+              });*/
+        }else{
+            alert("correo y/o contraseña Incorrecta");
+        }
+    });
+  }
+
   _onSubmit(){
     if(!this.usuario?.valid || this.usuario?.value==" ")return alert("Ingrese Correo Electronico");
      if(!this.password?.valid || this.password?.value==" ")return alert("Ingrese password");
     if(this.loginForm.valid){
-      alert("Bienvenido");
+      this.__login(this.loginForm.value);
     }else{
       alert("Rellene los datos");
     }
---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private readonly http:HttpClient
+  ) { }
+  __login(data:any){
+    return this.http.post<any>('https://localhost:44343/api/user/list',data);
+  }
+  __insert(data:any){
+    return this.http.post<any>('https://localhost:44343/api/user/create',data);
+
+  }
+}
