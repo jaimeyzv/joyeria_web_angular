71364e6276c125b301eeba22394ffe5622434e99
Alreyesh <alfreyh@gmail.com>
2022-02-18 07:32:34 -0500

    Update => users-update admin

--- src/app/user/list/users-update/users-update.component.ts
+++ src/app/user/list/users-update/users-update.component.ts
@@ -34,15 +34,14 @@ export class UsersUpdateComponent implements OnInit {
   }
  codigo: any="";
 updateForm = this.fb.group({
-    name:[this.user.name,[Validators.required,Validators.minLength(3)]],
-    lastName:['',[Validators.required,Validators.minLength(3)]],
-    documentTypeId:['',Validators.required],
-    documentNumber:['',Validators.required],
-    address:['',Validators.required],
-    cellphone:['',[Validators.required,Validators.min(9)]],
-    email:['',[Validators.required,Validators.email]],
-    password:['',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
-confirmpassword:['',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
+    name:['',[Validators.minLength(3)]],
+    lastName:['',[Validators.minLength(3)]],
+    documentTypeId:[''],
+    documentNumber:[''],
+    address:[''],
+    cellphone:['',[Validators.min(9)]],
+    email:['',[Validators.email]],
+    password:['',[Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
     userTypeId:['2']
   })
 
@@ -61,10 +60,6 @@ confirmpassword:['',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-
 
 _onSubmit(){
 
-  if(!this.name?.valid|| this.name?.value==" ")return alert("El campo Nombres esta vacio o tiene un minimo de caracteres");
-  if(!this.lastName?.valid|| this.lastName?.value==" ")return alert("El campo Apellidos esta vacio o tiene un minimo de caracteres");
-  if(!this.documentTypeId?.valid)return alert("No Selecciono el Tipo de Documento de Identidad");
-  if(!this.documentNumber?.valid|| this.documentNumber?.value==" ")return alert("Ingrese un Numero de Documento de Identidad");
 let number = this.updateForm.controls['documentNumber'].value?.length;
   if(this.documentTypeId?.value=="1"){
     //dni
@@ -81,22 +76,22 @@ if(number!=12)return alert("El Pasaporte debe tener 12 caracteres");
 //carnet Extranjeria
 if(number!=12)return alert("El Carnet de Extranjeria debe tener 12 caracteres");
   }
-  //if(!this.email?.valid|| this.email?.value==" ")return alert("Ingrese un correo electronico correcto");
-  if(!this.cellphone?.valid|| this.cellphone?.value==" ")return alert("Ingrese numero de celular");
-  if(!this.address?.valid|| this.address?.value==" ") return alert("Ingrese una direccion");
-  if(!this.password?.valid|| this.password?.value==" ")return alert('Ingrese una contraseña con lo siguiente:\n \tAl menos 8 caracteres de longitud\n\tMinimo una letra minusculas\n\tMinimo una letra mayúscula\n\tMinimo un número de Caracter especial');
- // if(!this.confirmemail?.valid|| this.confirmemail?.value==" ")return alert("Confirme su correo electronico");
-  if(!this.confirmpassword?.valid|| this.confirmpassword?.value==" ")return alert("Confirme su contraseña");
-  //if(this.email?.value != this.confirmemail?.value)return alert("el correo de confirmación no coinciden");
-  if(this.password?.value != this.confirmpassword?.value)return alert("La contraseña de confirmación no coinciden");
+  if(!this.password?.valid)return alert('Ingrese una contraseña con lo siguiente:\n \tAl menos 8 caracteres de longitud\n\tMinimo una letra minusculas\n\tMinimo una letra mayúscula\n\tMinimo un número de Caracter especial');
+
+  //if(!this.confirmpassword?.valid)return alert("Confirme su contraseña");
+  //if(this.password?.value != this.confirmpassword?.value)return alert("La contraseña de confirmación no coinciden");
+
+ this._update(this.updateForm.value,this.codigo);
+ /*
   if(this.updateForm.valid ){
     let nombre = this.name?.value;
     let apellido=this.lastName?.value;
 
-   this._update(this.updateForm.value,this.codigo);
+
   }else{
     alert("Rellene los datos");
   }
+  */
 }
 
 
@@ -135,7 +130,7 @@ _update(data:any,id:any){
 __getUserById(codigo:number){
   this.us._getUserById().subscribe((rest:any)=>{
     this.user = rest.filter((item:{id:number})=> item.id ==codigo);
-
+    console.log(this.user);
 
   })
 }
